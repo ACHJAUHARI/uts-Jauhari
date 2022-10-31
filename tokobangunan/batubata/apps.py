ae2345da690c7955ae5aba9a05873760797288df
ACHJAUHARI <107332310+ACHJAUHARI@users.noreply.github.com>
2022-10-31 17:23:12 +0700

    Add files via upload

---
+++ tokobangunan/batubata/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BatubataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'batubata'
---
+++ tokobangunan/genting/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GentingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'genting'
